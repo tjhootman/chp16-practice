b6f272a97c9fbb9cc8d945bb8ea34438a5061d77
tjhootman <tjhootman@gmail.com>
2025-04-26 10:16:17 -0400

    Extract earthquake magnitude data to dictionary

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -5,6 +5,12 @@ path = Path('eq_data/eq_data_1_day_m1.geojson')
 contents = path.read_text(encoding='utf-8')
 all_eq_data = json.loads(contents)
 
-path = Path('eq_data/readable_eq_data.geojson')
-readable_contents = json.dumps(all_eq_data, indent=4)
-path.write_text(readable_contents)
+# Examine all earthquakes in the dataset.
+all_eq_dicts = all_eq_data['features']
+
+mags = []
+for eq_dict in all_eq_dicts:
+    mag = eq_dict['properties']['mag']
+    mags.append(mag)
+
+print(mags[:10])
