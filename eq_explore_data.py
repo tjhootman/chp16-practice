680570b40b7fd7978ec53d8b58156ddf7a27f76b
tjhootman <tjhootman@gmail.com>
2025-04-26 10:18:07 -0400

    Extract long, lat data to dictionary

--- eq_explore_data.py
+++ eq_explore_data.py
@@ -8,9 +8,15 @@ all_eq_data = json.loads(contents)
 # Examine all earthquakes in the dataset.
 all_eq_dicts = all_eq_data['features']
 
-mags = []
+mags, lons, lats = [], [], []
 for eq_dict in all_eq_dicts:
     mag = eq_dict['properties']['mag']
+    lon = eq_dict['geometry']['coordinates'][0]
+    lat = eq_dict['geometry']['coordinates'][1]
     mags.append(mag)
+    lons.append(lon)
+    lats.append(lat)
 
 print(mags[:10])
+print(lons[:5])
+print(lats[:5])
